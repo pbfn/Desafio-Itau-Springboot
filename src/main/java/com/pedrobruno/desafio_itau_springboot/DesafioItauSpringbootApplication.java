5ec14554cc96693113180229f195c19080705c2a
Pedro Bruno Fernandes <pedro.nascimento@maximatech.com.br>
2025-03-16 11:25:27 -0300

    first commit

---
+++ src/main/java/com/pedrobruno/desafio_itau_springboot/DesafioItauSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.pedrobruno.desafio_itau_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioItauSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedrobruno/desafio_itau_springboot/DesafioItauSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedrobruno.desafio_itau_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
