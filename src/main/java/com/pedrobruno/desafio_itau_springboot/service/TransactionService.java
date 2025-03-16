b7416efc50c66f597ae78ac429bf278d178230f7
Pedro Bruno Fernandes <pedro.nascimento@maximatech.com.br>
2025-03-16 12:18:51 -0300

    Criado o service da transação

---
+++ src/main/java/com/pedrobruno/desafio_itau_springboot/service/TransactionService.java
@@ -0,0 +1,34 @@
+package com.pedrobruno.desafio_itau_springboot.service;
+
+import com.pedrobruno.desafio_itau_springboot.model.Transaction;
+import org.springframework.stereotype.Service;
+
+import java.time.OffsetDateTime;
+import java.util.DoubleSummaryStatistics;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
+@Service
+public class TransactionService {
+
+    private final Queue<Transaction> transactions = new ConcurrentLinkedDeque<>();
+
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    public void clearTransactions() {
+        transactions.clear();
+    }
+
+    public DoubleSummaryStatistics getStatistics() {
+        OffsetDateTime now = OffsetDateTime.now();
+
+        return transactions.stream()
+                .filter(t -> t.getDataHora().isAfter(now.minusSeconds(60)))
+                .mapToDouble(Transaction::getValor)
+                .summaryStatistics();
+    }
+
+}
