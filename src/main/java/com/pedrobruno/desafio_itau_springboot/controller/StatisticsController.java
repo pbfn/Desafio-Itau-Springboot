2877a62297ea6fd5e0629901d603540269b5cd23
Pedro Bruno Fernandes <pedro.nascimento@maximatech.com.br>
2025-03-16 12:19:40 -0300

    Criado endpoints para listar as transações.

---
+++ src/main/java/com/pedrobruno/desafio_itau_springboot/controller/StatisticsController.java
@@ -0,0 +1,26 @@
+package com.pedrobruno.desafio_itau_springboot.controller;
+
+import com.pedrobruno.desafio_itau_springboot.dto.StatisticsResponse;
+import com.pedrobruno.desafio_itau_springboot.service.TransactionService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.DoubleSummaryStatistics;
+
+@RestController
+@RequestMapping("/estatistica")
+public class StatisticsController {
+    private final TransactionService transactionService;
+
+    public StatisticsController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping
+    public ResponseEntity<StatisticsResponse> getStatistics() {
+        DoubleSummaryStatistics stats = transactionService.getStatistics();
+        return ResponseEntity.ok(new StatisticsResponse(stats));
+    }
+}
